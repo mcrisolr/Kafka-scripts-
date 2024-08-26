65ae56f7e47b8b467ce9f01427e985728e730f73
Miguel Crisol <66435909+mcrisolr@users.noreply.github.com>
2024-08-26 19:53:37 +0200

    Update kafka_consumers_per_topic_list.sh

--- kafka_consumers_per_topic_list.sh
+++ kafka_consumers_per_topic_list.sh
@@ -1,9 +1,7 @@
 #/bin/bash
-#Get all consumers-per-topic as a table of topictabconsumer 
 
-set -x 
+set -v -x -e 
 
 for t in `kafka-consumer-groups --bootstrap-server localhost:9092 --list 2>/dev/null`; do
-    echo $t | xargs -I {} sh -c "kafka-consumer-groups --bootstrap-server localhost:9092 --describe --group {} 2>/dev/null | grep ^{} | awk '{print \$2\"\t\"\$1}' "
+    echo $t | xargs -I {} sh -c "kafka-consumer-groups --bootstrap-server localhost:9092 --describe --group {} 2>/dev/null | grep ^{} | awk '{print \$2\"\t\"\$1\"\t\"\$7\"\t\"\$8}' "
 done > topics-consumer.txt
-
